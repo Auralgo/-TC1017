dcf1f1bb9e83f73d450f0eb5c91d7c08e3eef2b9
Aurora <A01630329@itesm.mx>
2015-02-27 15:30:49 -0600

    New File WSQ05

---
+++ Temperature.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int temperature, celsius;
+	cout << "Write the temperature in F" << endl;
+	cin >> temperature;
+	celsius = 5*(temperature-32)/9;
+	cout << "Your temperature in celsius is " << celsius << endl;
+	if(celsius >= 100){
+			cout << "The water is just boiling" << endl;
+	} else{
+		cout <<"The water would not boil" << endl;
+	}
+}
