bd1b6af226e7eb6a7e087920a73e1e6fda84a7cd
Aurora <A01630329@itesm.mx>
2015-04-04 23:48:28 -0600

    Quiz7

---
+++ q1quiz7.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+float calcular_promedio (float array [], int size) {
+  int i;
+  float promedio;
+  int suma = 0;
+  for (int i = 0; i <= size; i++){
+    suma = suma + array[i];
+
+  }
+  promedio = suma/size;
+  return promedio;
+}
+
+
+
+int calcular_suma(float a[], int size){
+  int total = 0;
+  for (int i = 0; i < size; i++){
+       total = total + a[i];
+  }
+  return total;
+}
+
+int calcular_desviacion(float sum, float promedio, int size, float array[]) {
+  //float deviation;
+
+
+for (int i = 0; i < size; i++){
+
+  sum=sum+pow(array[i]-promedio,2);
+
+}
+
+return sqrt(sum/size);
+
+
+
+  }
+
+int main(){
+  float promedio;
+  float sum;
+  float suma;
+  float deviation;
+  int size = 5;
+  float array[size];
+
+  for (int i = 0; i <size; i++){
+    cout << "Give me the variables" << endl;
+    cin >> array[i];
+  }
+
+  suma = calcular_suma(array,size);
+  promedio = calcular_promedio(array, size);
+  deviation = calcular_desviacion(sum,promedio,size,array);
+  cout <<"The sum of the numbers is " << suma << endl;
+  cout << "The average of the numbers is " << promedio << endl;
+  cout << "The standard deviation is " << deviation << endl;
+
+  return 0;
+}
